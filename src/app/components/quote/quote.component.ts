1149b84afea036cedc3440c492fdb041a17e2791
AtienoObwanda <atienoobwanda@gmail.com>
2022-04-09 04:03:55 +0300

    Added UI, and add quote function

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -7,14 +7,24 @@ import {Quotes} from '../../model/Quotes';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  inputQuote :string = "";
+  inputAuthor:string="";
+  inputPublisher:string="";
 //Array for quotes
-quotes : Quotes[]=[
-  new Quotes('Sometimes.','Steve Jobs','Atieno',2,1),
-  new Quotes('Sometimes.','Steve Jobs','Mishie',2,1)
-]
+quotes !: Quotes[]
   constructor() { }
 
   ngOnInit(): void {
+    this.quotes = [
+      new Quotes('Sometimes.','Steve Jobs','Atieno',2,1),
+      new Quotes('Sometimes.','Steve Jobs','Mishie',2,1)
+    ]
+
+  }
+
+  addQuote(){
+    this.quotes.push()
+
   }
 
 }
