f475faf254d436853185ac9afb9f5833800b35ca
AtienoObwanda <atienoobwanda@gmail.com>
2022-04-09 03:34:39 +0300

    Added the quotes model

---
+++ src/app/model/Quotes.spec.ts
@@ -0,0 +1,7 @@
+import { Quotes } from './Quotes';
+
+describe('Quotes', () => {
+  it('should create an instance', () => {
+    expect(new Quotes()).toBeTruthy();
+  });
+});
---
+++ src/app/model/Quotes.ts
@@ -0,0 +1,14 @@
+export class Quotes{
+
+    constructor(
+        public quoteTitle: string,
+        public author: string,
+        public publisher : string,
+        public likes:number,
+        public dislikes:number
+
+    )
+    {
+       // this.show = false
+    }
+}
